7d6622b6c1e82893c915b45826739418ffd57dc3
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2024-06-04 20:19:18 -0300

    Create lib/core and move non-Rails components

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module SolidRailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths << Rails.root.join("lib/core")
   end
 end
